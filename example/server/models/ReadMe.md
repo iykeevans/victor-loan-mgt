6d3c05f4a912ba08e000527ef610934a2789caad
saladinjake <juwavictor@gmail.com>
2024-11-26 21:10:29 +0000

    chore: model workspace and user or employee entity model

---
+++ example/server/models/ReadMe.md
@@ -0,0 +1,11 @@
+
+Soft ware as a service approach
+// kkeep det to ur self
+Company can subscribe to a work space via a plan with x numbers of users
+Company Can choose the plugin features or modules for their day to day collaboration 
+  -- Plugins ca include
+      -- Spark - Messaging Channel
+      -- Agile - Project Managemt Tool
+      -- Kitana - Notion Board for Road maps and Goals
+      --Ruth - Human Resource and Employee management Tool
+      -- Spark Plug -  Employee wellness Perk Box  
---
+++ example/server/models/userModel.ts
@@ -0,0 +1,72 @@
+import { Schema, model, Document } from 'mongoose';
+import bcrypt from "bcrypt"
+import mongoose, { ObjectId } from 'mongoose';
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  accessToken: string;
+  refreshToken: string;
+  scope: string;
+  state: string;
+  userID: string;
+  sessionExpiresAt: Date;
+  otp: string;
+  otpExpiresAt: Date;
+  isVerified: boolean;
+  apiKey: string;
+  lastApiCall: Date;
+  lastApiCallCount: number;
+  roles: mongoose.Types.ObjectId[];
+  permissions: any[];
+  subscriptions:  mongoose.Types.ObjectId[];
+  userFunctions:mongoose.Types.ObjectId[];
+  subscriptionExpiresAt: Date;
+  workspaces: mongoose.Types.ObjectId[]; // Reference to workspaces the user is part of
+}
+
+const userSchema = new Schema<IUser>({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  accessToken: { type: String },
+  refreshToken: { type: String },
+  scope: { type: String },
+  state: { type: String },
+  userID: { type: String, required: true },
+  sessionExpiresAt: { type: Date, required: true },
+  otp: { type: String },
+  otpExpiresAt: { type: Date },
+  isVerified: { type: Boolean, default: false },
+
+  apiKey: { type: String, required: true }, // API key
+  subscriptions: [{ type:  mongoose.Schema.Types.ObjectId, ref: 'Plan' }], // Reference to subscribed plans
+  subscriptionExpiresAt: { type: Date }, // The expiration of the subscription (if applicable)
+  lastApiCall: { type: Date },
+  lastApiCallCount: { type: Number, required: true, default: 0 },
+  roles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Role' }],  // User has roles
+  permissions: [{ type: String }],  // You can store permissions here if needed
+  workspaces: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workspace' }],
+  userFunctions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserFunction' }]  // List of UserFunction references
+}, { timestamps: true });
+
+userSchema.pre<IUser>('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+
+// Add a method to the User schema to fetch permissions based on roles
+userSchema.methods.assignPermissions = async function () {
+  // Aggregate all permissions based on the roles assigned to the user
+  const roles = await mongoose.model('Role').find({ '_id': { $in: this.roles } }).populate('permissions');
+  this.permissions = roles.reduce((acc, role) => {
+    return [...acc, ...role.permissions];
+  }, []);
+  await this.save();  // Save permissions to the user document
+};
+
+const User = model<IUser>('User', userSchema);
+
+export default User;
